32c4d74387148d1ed205813ee36999d33c5693ef
ayfor <josh.a.stubbington@gmail.com>
2023-02-02 22:10:58 -0500

    generated ingredients controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'ingredients#index'
+  get '/ingredients', to: 'ingredients#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
