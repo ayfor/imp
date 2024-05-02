57311ed1afdbd7c7fac876fdcf313b21a4eeeb52
ayfor <josh.a.stubbington@gmail.com>
2024-05-02 17:30:00 -0400

    Refactor Ingredient model and form

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,11 @@
 class Ingredient < ApplicationRecord
-  self.UNITS = %w[piece g kg ml l].freeze
+  @units = %w[piece g kg ml l].freeze
+
+  def self.units
+    @UNITS
+  end
+
+  validates :name, presence: true
+  validates :unit, inclusion: { in: @units }
+  validates :quantity, numericality: { greater_than: 0 }
 end
