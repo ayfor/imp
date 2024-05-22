6a75c7b3857502d0a2f6858ad18917d8874899a1
ayfor <josh.a.stubbington@gmail.com>
2024-05-21 22:37:27 -0400

    styling and finished hooking up the ingredients new form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   @@registered_button_styles = {
     primary: 'rounded-md bg-leftovers-blue px-3 py-2 text-sm font-medium text-white shadow-sm hover:bg-leftovers-blue/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-leftovers-blue',
+    alternate: 'rounded-md bg-white px-3 py-2 text-sm font-medium text-leftovers-blue border-2 border-leftovers-blue shadow-sm hover:text-white hover:bg-leftovers-blue/80 hover:border-leftovers-blue/70 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-leftovers-blue',
     rounded: 'rounded-full bg-leftovers-blue px-3.5 py-2 text-sm font-medium text-white shadow-sm hover:bg-leftovers-blue/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-leftovers-blue',
     white: 'rounded-md bg-white px-3 py-2 text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50',
     wide: 'inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
@@ -15,6 +16,9 @@ module ApplicationHelper
             end
 
     style << ' inline-flex items-center justify-center' if obj.key?(:is_link) && obj[:is_link]
+
+    style << ' mx-1 cursor-pointer'
+
     style
   end
 
